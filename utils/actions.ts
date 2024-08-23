0657f5188daf0cbb05a06e7a6f88a3aa1ae1f82c
quyenhoang140299 <hoangquyen140299@gmail.com>
2024-08-23 17:07:48 +0700

    add supabase

--- utils/actions.ts
+++ utils/actions.ts
@@ -1,5 +1,23 @@
+"use server";
 import { redirect } from "next/navigation";
 import db from "./db";
+import { auth, currentUser } from "@clerk/nextjs/server";
+import { imageSchema, productSchema, validateWithZodSchema } from "./schemas";
+
+const renderError = (error: unknown) => {
+    console.log(error);
+    return {
+        message: error instanceof Error ? error.message : "An error occurred",
+    };
+};
+
+const getAuthUser = async () => {
+    const user = await currentUser();
+    if (!user) {
+        throw new Error("You must bbe logged in to process this route");
+    }
+    return user;
+};
 
 export const fetchFeaturedProducts = async () => {
     const products = await db.product.findMany({
@@ -32,4 +50,30 @@ export const fetchSingleProduct = async (productId: string) => {
     });
     if (!product) redirect("/");
     return product;
+};
+
+export const createProductAction = async (
+    prevState: any,
+    formData: FormData
+): Promise<{ message: string }> => {
+    try {
+        const user = await getAuthUser();
+        const file = formData.get("image") as File;
+        const rawData = Object.fromEntries(formData);
+        const validateFile = validateWithZodSchema(imageSchema, {
+            image: file,
+        });
+        const validatedFields = validateWithZodSchema(productSchema, rawData);
+
+        await db.product.create({
+            data: {
+                ...validatedFields,
+                image: "validateFile.image",
+                clerkId: user.id,
+            },
+        });
+        return { message: "Product created" };
+    } catch (error) {
+        return renderError(error);
+    }
 };
