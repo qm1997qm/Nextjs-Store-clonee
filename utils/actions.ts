c94a23001b086d91ef6cfd1aa6fd8363205c2954
quyenhoang140299 <hoangquyen140299@gmail.com>
2024-08-25 01:14:04 +0700

    why?

--- utils/actions.ts
+++ utils/actions.ts
@@ -477,7 +477,7 @@ export const addToCartAction = async (prevState: any, formData: FormData) => {
     try {
         const productId = formData.get("productId") as string;
         const amount = Number(formData.get("amount"));
-        // await fetchProduct(productId);
+        // await fetchProduct(productId);/////// error
         const cart = await fetchOrCreateCart({ userId: user.id });
         await updateOrCreateCartItem({ productId, cartId: cart.id, amount });
         await updateCart(cart);
