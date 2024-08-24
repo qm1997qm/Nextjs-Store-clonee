5671a926233b972dbc4bc2c4173609dd92404166
quyenhoang140299 <hoangquyen140299@gmail.com>
2024-08-24 23:53:36 +0700

    test

--- components/form/Buttons.tsx
+++ components/form/Buttons.tsx
@@ -23,23 +23,24 @@ export function SubmitButton({
     text = "submit",
     size = "lg",
 }: SubmitButtonProps) {
-    const { pending } = useFormStatus();
+    // const { pending } = useFormStatus();
 
     return (
         <Button
             type='submit'
-            disabled={pending}
+            // disabled={pending}
             className={cn("capitalize", className)}
             size={size}
         >
-            {pending ? (
+            {text}
+            {/* {pending ? (
                 <>
                     <ReloadIcon className='mr-2 h-4 w-4 animate-spin' />
                     Please wait...
                 </>
             ) : (
                 text
-            )}
+            )} */}
         </Button>
     );
 }
