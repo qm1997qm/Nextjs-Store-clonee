d1c52aa233b56c2f8a0a02762827ec8221148ccd
quyenhoang140299 <hoangquyen140299@gmail.com>
2024-08-24 23:33:35 +0700

    fix

--- components/cart/CartTotals.tsx
+++ components/cart/CartTotals.tsx
@@ -1,7 +1,6 @@
 import { Card, CardTitle } from "@/components/ui/card";
 import { Separator } from "@/components/ui/separator";
 import { formatCurrency } from "@/utils/format";
-import { createOrderAction } from "@/utils/actions";
 import FormContainer from "../form/FormContainer";
 import { SubmitButton } from "../form/Buttons";
 import { Cart } from "@prisma/client";
@@ -22,9 +21,6 @@ function CartTotals({ cart }: { cart: Cart }) {
                     />
                 </CardTitle>
             </Card>
-            <FormContainer action={createOrderAction}>
-                <SubmitButton text='Place Order' className='w-full mt-8' />
-            </FormContainer>
         </div>
     );
 }
